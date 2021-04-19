9eb50d937325e5bba6b1358dff0de11eabfcc0ea
Idorenyin <drenoaka2003@gmail.com>
2021-04-19 01:44:41 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+
+      colors: {
+        secondary: '#2465a5',
+        fidicol:'#0E3964'
+      },
+
+      width: {
+
+        111: '40em',
+        104: '33.5em',
+        109: '21em',
+        190:'18.8em',
+        199:'61.5em',
+        144:'17.5em',
+        155:'78.9vw'
+      }
+
+    },
+  },
+  variants: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
